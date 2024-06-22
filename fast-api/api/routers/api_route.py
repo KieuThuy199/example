ac02fc7d97af190139c0359f2915adddfd6fe03d
kthuy-duong <kieu-d@dymvietnam.jp>
2024-06-22 15:51:08 +0700

    setup src

---
+++ fast-api/api/routers/api_route.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, HTTPException
+from bson import ObjectId
+
+from database import get_collection
+from models import Item, ItemInDB, serialize_items, serialize_item
+
+route = APIRouter()
+news_collection = get_collection()
+
+
+@route.get("/newses/", response_model=list[ItemInDB])
+async def index():
+    result = list(news_collection.find())
+    return serialize_items(result)
+
+
+@route.get("/newses/{news_id}", response_model=ItemInDB | str)
+async def show(news_id: str):
+    item = news_collection.find_one({"_id": ObjectId(news_id)})
+    if item:
+        return serialize_item(item)
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+@route.post("/newses/", response_model=ItemInDB)
+async def add(data: Item):
+    item_id = news_collection.insert_one(data.dict()).inserted_id
+    item = news_collection.find_one({"_id": item_id})
+    return serialize_item(item)
+
+
+@route.put("/newses/{news_id}")
+async def update(news_id: str, data: Item):
+    result = news_collection.update_one({"_id": ObjectId(news_id)}, {"$set": data.dict()})
+    if result.matched_count == 0:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"message": "Updated item successfully"}
+
+
+@route.delete("/newses/{news_id}")
+async def delete(news_id: str):
+    result = news_collection.delete_one({"_id": ObjectId(news_id)})
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"message": "Deleted item successfully"}
